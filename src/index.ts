6779a9461a2cd6e525aa96e6ef8d4dc5d909cfaf
Alex Santos de Souza <alexssmusica@gmail.com>
2024-10-29 21:21:17 -0300

    v1.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { gerarPDF } from '@alexssmusica/node-pdf-nfe';
-import { setupLoadCertificateFromBase64 } from './domain/use-cases/load-certificate-from-base64';
-import { setupLoadCertificateFromPath } from './domain/use-cases/load-certificate-from-path';
+export { gerarPDF } from '@alexssmusica/node-pdf-nfe';
+export { cancelar } from './domain/use-cases/processo/cancelar';
 export { cartaCorrecao } from './domain/use-cases/processo/carta-correcao';
 export { emitir } from './domain/use-cases/processo/emissao';
 export { inutilizar } from './domain/use-cases/processo/inutiliza';
 export { statusServico } from './domain/use-cases/processo/status';
 export * from './domain/use-cases/utils';
-export { carregaCertificadoBase64, carregaCertificadoPath, gerarPDF };
+import { setupLoadCertificateFromBase64 } from './domain/use-cases/load-certificate-from-base64';
+import { setupLoadCertificateFromPath } from './domain/use-cases/load-certificate-from-path';
 const carregaCertificadoBase64 = setupLoadCertificateFromBase64();
 const carregaCertificadoPath = setupLoadCertificateFromPath();
+
+export { carregaCertificadoBase64, carregaCertificadoPath };
